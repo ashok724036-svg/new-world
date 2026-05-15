fbf8cf1caa77cd7a062e5113be66b52456b1ed64
ashok724036-svg <ashok724036@gmail.com>
2026-05-15 07:17:11 +0530

    copy: gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@echo off
+  set APP_HOME=%~dp0
+  set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+  set JAVACMD=java
+  if defined JAVA_HOME set JAVACMD=%JAVA_HOME%\bin\java.exe
+  "%JAVACMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+  
